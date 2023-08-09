dc42af86f3c350411d271abf73684a01a2a638bd
Aadi Johari <zakethegr8@gmail.com>
2023-08-09 18:47:12 -0500

    Add formatting + Ebay support

--- web_scraper.py
+++ web_scraper.py
@@ -1,18 +1,63 @@
 from bs4 import BeautifulSoup
 import requests
 import re
+from urllib.parse import urlparse
 
+def ebay_scraper(url, document):
+    title = document.select("h1.x-item-title__mainTitle")[0].text.strip()
+    title = re.split(" - | AMD| Intel| Geforce| Nvidia| Windows", title)[0]
+    
+    find_text_by_parent = lambda parent, all_words: all_words[0].parent.find(parent).text
+    
+    words = document.select("div.x-price-primary")[0].text.strip()
+    words = words.split("US ")[1].strip("$")
+    
+    true_price = float("".join(d for d in words if d != ","))
+    
+    return title, words, true_price
+    
+    
+def newegg_scraper(url, document):
+    title = document.select("h1.product-title")[0].text.strip()
+    title = re.split(" - | AMD| Intel| Geforce| Nvidia| Windows", title)[0]
+    
+    find_text_by_parent = lambda parent, all_words: all_words[0].parent.find(parent).text
+    
+    words = document.find_all(string=re.compile("$"))
+    dollar = find_text_by_parent("strong", words)
+    cents = find_text_by_parent("sup", words)
+    true_price = float("".join(d for d in dollar if d != ",") + cents)
+    
+    return title, dollar + cents, true_price
+    
+    
 def main():
-    url = "NEWEGG URL HERE"
+    url = input("Enter URL\n\n> ")
+    domain = urlparse(url).netloc
+    if domain == "www.ebay.com":
+        choice = 1
+    elif domain == "www.newegg.com":
+        choice = 2
+    else:
+        print("Invalid URL")
+        return
     result = requests.get(url)
     doc = BeautifulSoup(result.text, "html.parser")
-    
-    words = doc.find_all(string=re.compile("$"))
-    dollar = words[0].parent.find("strong")
-    cents = words[0].parent.find("sup")
-    print(dollar.text + cents.text)
+    try:
+        if choice == 1:
+            title, formatted, true = ebay_scraper(url, doc)
         
+        if choice == 2:
+            title, formatted, true = newegg_scraper(url, doc)
+    except Exception:
+        return
+    
+    print(f'Price for "{title}" on {domain} is ${formatted}')
 
 
 if __name__ == '__main__':
-    main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("\n\nExiting...")
+        exit()
